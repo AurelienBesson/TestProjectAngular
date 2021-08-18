55f3ec9bf24778a34065cb5b825c85a35887a4f1
AurelienBesson <aurelien.besson@outlook.com>
2021-08-18 19:56:31 +0200

    Update app.module.ts

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 @NgModule({
     imports: [ BrowserModule ],
     declarations: [ AppComponent ],
-    bootstrap: [ AppComponent ]
+    bootstrap: [ AppComponent ],
+    exports: [ AppComponent ]
 })
 export class AppModule { }
